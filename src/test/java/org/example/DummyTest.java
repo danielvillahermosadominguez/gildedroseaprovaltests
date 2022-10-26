f7fcdf8593944338183032a8e67d0e441c792a49
Daniel <daniel.villahermosa@codurance.com>
2022-10-26 11:45:55 +0200

    empty project with dummy test

---
+++ src/test/java/org/example/DummyTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+    @Test
+    void dummy_test() {
+        assertTrue(true);
+    }
+}
