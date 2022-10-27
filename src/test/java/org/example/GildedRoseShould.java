cb8e92332d0e2472795c0398a5c3a20069e7f3fe
Daniel <daniel.villahermosa@codurance.com>
2022-10-27 15:36:54 +0200

    dummy test with approval tests

---
+++ src/test/java/org/example/GildedRoseShould.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.DiffReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+@UseReporter(DiffReporter.class)
+public class GildedRoseShould {
+   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void dummy_test() {
+        System.out.println("dummy");
+        String output = outContent.toString();
+        Approvals.verify(output);
+    }
+}
