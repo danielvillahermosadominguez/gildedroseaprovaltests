7628a417d301d57610dde2d31b8f5969b06e935b
Daniel <daniel.villahermosa@codurance.com>
2022-10-31 12:00:06 +0100

    simplification of the ifs in three main cases

--- src/main/java/org/example/GildedRose.java
+++ src/main/java/org/example/GildedRose.java
@@ -42,27 +42,20 @@ class GildedRose {
                     if (items[i].sellIn < 0) {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                if (true) {
-                    if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
+            } else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
                 }
 
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].sellIn = items[i].sellIn - 1;
-                }
+                items[i].sellIn = items[i].sellIn - 1;
 
                 if (items[i].sellIn < 0) {
                     if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
+                        items[i].quality = items[i].quality - 1;
                     }
                 }
-                }
-            }
+            } 
         }
+    }
+
 }
